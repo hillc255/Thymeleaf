335a7e50a0d6c48debd3e41845785839bc1d0153
Claudia Hill <hillc255@yahoo.com>
2019-09-25 05:48:30 -0400

    Add controller methods, demo2 template

--- Thymeleaf/src/main/java/com/example/Thymeleaf/controller/UserController.java
+++ Thymeleaf/src/main/java/com/example/Thymeleaf/controller/UserController.java
@@ -1,29 +1,72 @@
 package com.example.Thymeleaf.controller;
 
+import com.example.Thymeleaf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
 //http://localhost:8080/demo
-//Result on browser:  Hello Thymeleaf
 //https://www.thymeleaf.org/doc/tutorials/3.0/thymeleafspring.html
 //add dependency to pom file
 
+//Result on browser
 
+ /*     Hello Thymeleaf
+        Exam Results
+        You passed the exam.
+        You are Excellent  */
+
+//https://www.baeldung.com/spring-thymeleaf-fragments
 @Controller
 public class UserController {
 
-    @RequestMapping(value = "demo", method = RequestMethod.GET)
+    @RequestMapping("demo")
     public String demo(Model model) {
         model.addAttribute("message", "Hello Thymeleaf");
-
-        //add variables
-        // variable1
-        model.addAttribute("variable1", "Hyacinth");
-        // variable2
-        model.addAttribute("variable2", "Carnation");
+        double grade = 90.5;
+        model.addAttribute("grade", grade);
+        model.addAttribute("GPA", convertGPA(grade));
         // return to templates/demo.html page.
         return "demo";
+    }
+
+    private String convertGPA(double grade) {
+        if (grade >= 90) {
+            return "A";
+        } else if (grade < 90 && grade >= 80) {
+            return "B";
+        } else if (grade < 80 && grade >= 70) {
+            return "C";
+        } else if (grade < 70 && grade >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    @RequestMapping("demo2")
+    public String demo2(Model model) {
+        List<User> lst = new ArrayList<>();
+        lst.add(new User(1, "Tom", 30));
+        lst.add(new User(2, "Jerry", 29));
+        lst.add(new User(3, "Nancy", 27));
+        model.addAttribute("list", lst);
+        return "demo2";
+    }
+
+    @RequestMapping("demo3")
+    public String demo3(HttpServletRequest request, Model model) {
+        // Request
+        request.setAttribute("request", "request data");
+        // Session
+        request.getSession().setAttribute("session", "session data");
+        // Application
+        request.getSession().getServletContext().setAttribute("application", "application data");
+        return "demo2";
     }
 }
